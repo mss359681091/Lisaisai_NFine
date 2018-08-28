71d4c3cacde6edb7e09ef0ac7842a7ba80e85352
Administrator <Administrator@ZLQ81KFI5Y7IUIC>
2018-08-28 17:42:23 +0800

    更该botto

--- NFine.Web/Content/js/login.js
+++ NFine.Web/Content/js/login.js
@@ -112,7 +112,7 @@
                 dataType: "json",
                 success: function (data) {
                     if (data.state == "success") {
-                        window.location.href = "/ProjectData/ProjectInfo/Index";
+                        window.location.href = "/Home/Index";
                     }
                     else {
                         lrPage.logining(false);
